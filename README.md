11ff6d9183a429d39d9acf7560da437173888f47
ktabaczynski <kacpertabaczynski@icloud.com>
2022-03-17 21:14:26 +0100

    Updated version to merge files

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Guardicore-Exporter
 
 How-to for windows:
+
   Clone repository to your local machine
+
   Open cmd
+
   Run 'python3 --version' to make sure that python is installed
-  Run exporter with following command: python3 guardicore_export.py sample.csv output.csv
+  
+  Run exporter with following command: python3 guardicore_export.py input_file1.csv input_file2.csv output.file
   
 Enjoy!
--- guardicore_export.py
+++ guardicore_export.py
@@ -2,20 +2,25 @@ import csv
 import sys
 
 
-export_data=set()
+export_data = set()
 
-def open_file(mode='r'):
-    arg_pos= 2 if mode=='w' else 1
-    return open(sys.argv[arg_pos],mode,encoding='UTF8',newline='')
+def open_file(arg_pos, mode='r'):
+    return open(sys.argv[arg_pos], mode, encoding='UTF8', newline='')
 
-with open_file('r') as fr:
-    csvreader = csv.reader(fr)
-    for row in csvreader:
-      source_asset_name,src_ip,dest_asset_name,dest_ip,dest_port,dest_proto=row[7],row[5],row[16],row[14],row[23],row[24]
-      export_data.add((source_asset_name,src_ip,dest_asset_name,dest_ip,dest_port,dest_proto))
 
-with open_file('w') as fw:
+for i in range(1, len(sys.argv)-1):
+    with open_file(mode='r', arg_pos=i) as fr:        
+        csvreader = csv.reader(fr)
+        header = next(csvreader)
+        for row in csvreader:
+            if len(row) > 24:
+                source_asset_name, src_ip, dest_asset_name, dest_ip, dest_port, dest_proto = row[7], row[5], row[16], row[14], row[23], row[24]
+            else:
+                source_asset_name, src_ip, dest_asset_name, dest_ip, dest_port, dest_proto = row[0], row[1], row[2], row[3], row[4], row[5]            
+            export_data.add((source_asset_name, src_ip, dest_asset_name, dest_ip, dest_port, dest_proto))
+
+with open_file(mode='w', arg_pos=-1) as fw:
     writer = csv.writer(fw)
-    writer.writerow( ('source_asset_name', 'source_ip', 'destination_asset_name', 'destination_ip','destination_port','ip_protocol') )
+    writer.writerow( ('source_asset_name', 'source_ip', 'destination_asset_name', 'destination_ip', 'destination_port', 'ip_protocol') )
     for row in export_data:
-      writer.writerow(row)
+        writer.writerow(row)
